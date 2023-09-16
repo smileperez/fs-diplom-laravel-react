d4cc9e7260af1f996dc852ebebfa1ea87a699965
smileperez <aa.efimenko@efimen.ru>
2023-09-16 21:37:48 +0300

    Добавлен HeadlessUI и созданы страницы dashboard и tickets

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -4,16 +4,23 @@ import Tickets from "./views/Tickets";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
 import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
 
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Dashboard />
-    },
-    {
-        path: '/tickets',
-        element: <Tickets />
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Dashboard />
+            },
+            {
+                path: '/tickets',
+                element: <Tickets />
+            },
+        ]
     },
 
     {
