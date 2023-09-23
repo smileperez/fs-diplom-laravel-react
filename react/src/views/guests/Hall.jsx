48fc2db56e8a7e06a9e08e1616d56bf08100456c
smileperez <aa.efimenko@efimen.ru>
2023-09-23 19:21:10 +0300

    Добавлены заготовки под новые guest страницы

---
+++ react/src/views/guests/Hall.jsx
@@ -0,0 +1,5 @@
+export default function Hall() {
+  return (
+    <div>Hall</div>
+  )
+}
--- react/src/views/guests/Tickets.jsx
+++ react/src/views/guests/Tickets.jsx
@@ -6,13 +6,21 @@ export default function Tickets() {
             </nav>
 
             <main>
-                <section className="mt-8 h-100 bg-white">
+                <section className="mt-8 h-100 bg-white p-3.5">
                     <div className="flex">
                         <div>
                             Картинка
                         </div>
-                        <div>
-                            Звёздные войны XXIII: Атака клонированных клонов
+                        <div className="pl-4">
+                            <h2 className="text-base">
+                                Звёздные войны XXIII: Атака клонированных клонов
+                            </h2>
+                            <p className="text-sm mt-2.5">
+                                Две сотни лет назад малороссийские хутора разоряла шайка нехристей-ляхов во главе с могущественным колдуном.
+                            </p>
+                            <p className="text-sm mt-2.5">
+                                130 минут США
+                            </p>
                         </div>
                     </div>
                     <div>
@@ -22,13 +30,21 @@ export default function Tickets() {
                         Зал 2
                     </div>
                 </section>
-                <section className="mt-8 h-100 bg-white">
+                <section className="mt-8 h-100 bg-white p-4">
                     <div className="flex">
                         <div>
                             Картинка
                         </div>
                         <div>
-                            Звёздные войны XXIII: Атака клонированных клонов
+                            <h2>
+                                Альфа
+                            </h2>
+                            <p>
+                                20 тысяч лет назад Земля была холодным и неуютным местом, в котором смерть подстерегала человека на каждом шагу.
+                            </p>
+                            <p>
+                                96 минут Франция
+                            </p>
                         </div>
                     </div>
                     <div>
@@ -38,13 +54,21 @@ export default function Tickets() {
                         Зал 2
                     </div>
                 </section>
-                <section className="mt-8 h-100 bg-white">
+                <section className="mt-8 h-100 bg-white p-4">
                     <div className="flex">
                         <div>
                             Картинка
                         </div>
                         <div>
-                            Звёздные войны XXIII: Атака клонированных клонов
+                            <h2>
+                                Хищник
+                            </h2>
+                            <p>
+                                Самые опасные хищники Вселенной, прибыв из глубин космоса, высаживаются на улицах маленького городка, чтобы начать свою кровавую охоту. Генетически модернизировав себя с помощью ДНК других видов, охотники стали ещё сильнее, умнее и беспощаднее.
+                            </p>
+                            <p>
+                                101 минута Канада, США
+                            </p>
                         </div>
                     </div>
                     <div>
