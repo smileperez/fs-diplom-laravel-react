63ad2e90a79e1779b257960f5a530b115f666d5b
smileperez <aa.efimenko@efimen.ru>
2023-09-23 13:56:10 +0300

    Доработка signin и signup 2

---
+++ react/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+export default function Logo() {
+    return (
+        <>
+            <h1 className="logo-head-01">
+                ИДЁМ
+                <span className="logo-head-02">В</span>
+                КИНО
+            </h1>
+            <h2 className="logo-head-03">Администраторская</h2>
+        </>
+    )
+}
---
+++ react/src/views/auth/Signin.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom"
+
+export default function Login() {
+    return (
+        <div className="p-5 bg-[#eae9eb]">
+            <form className="space-y-6" action="#" method="POST">
+                <div>
+                    <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                        E-mail
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            autoComplete="email"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <div className="flex items-center justify-between">
+                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                            Пароль
+                        </label>
+                    </div>
+                    <div className="mt-2">
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            autoComplete="current-password"
+                            required
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <button
+                        type="submit"
+                        className="flex w-full justify-center rounded-md bg-[#63536C] px-3 py-1.5 text-m font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 active:bg-[#89639e] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Вход
+                    </button>
+                </div>
+            </form>
+
+            <p className="mt-7 text-center text-sm text-gray-500">
+                Не зарегистрированы?{' '}
+                <Link to='/auth/signup' className="text-[#63536C] font-semibold hover:text-[#89639e]">
+                    Регистрация
+                </Link>
+            </p>
+        </div>
+    )
+}
