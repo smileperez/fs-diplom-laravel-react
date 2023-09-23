07ccf4aedeb8cc9f011058aef3e4c56fd8348005
smileperez <aa.efimenko@efimen.ru>
2023-09-23 14:54:00 +0300

    Работа на страницей Tickets 2

---
+++ react/src/components/LogoAdminComponent.jsx
@@ -0,0 +1,10 @@
+import LogoComponent from "./LogoComponent"
+
+export default function LogoAdminComponent() {
+    return (
+        <>
+            <LogoComponent />
+            <h2 className="logo-head-03">Администраторская</h2>
+        </>
+    )
+}
