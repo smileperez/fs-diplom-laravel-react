598e71e23711e212fcb8b73a3561bde943481aa0
smileperez <aa.efimenko@efimen.ru>
2023-09-24 00:53:04 +0300

    Установка и настройка axios. Создание request и response. Создание контроллера и API request 2

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SigninRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+
+        /** @var \App\Models\User $user */
+        $user = User::create([
+            'login' => $data['login'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function signin(SigninRequest $request)
+    {
+        $credentials = $request->validated();
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if (!Auth::attempt($credentials, $remember)) {
+            return response([
+                'error' => 'Данные не подходят'
+            ], 422);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function signout(Request $request)
+    {
+        /** $var user $user */
+        $user = Auth::user();
+        $user->currentAccessToken()-delete();
+
+        return response([
+            'success' => true
+        ]);
+    }
+}
