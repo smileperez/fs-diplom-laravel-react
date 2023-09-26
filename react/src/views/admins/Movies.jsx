e5d0c4bfb0fe345f7195cf235b23ce237a8f290c
smileperez <aa.efimenko@efimen.ru>
2023-09-26 12:35:24 +0300

    Добавлена страницы Movies

---
+++ react/src/views/admins/Movies.jsx
@@ -0,0 +1,11 @@
+import PageComponent from "../../components/PageComponent";
+
+export default function Movies() {
+    return (
+        <PageComponent title="Управление фильмами">
+            <div>
+                Movies content
+            </div>
+        </PageComponent>
+    )
+}
