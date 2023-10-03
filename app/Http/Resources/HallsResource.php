0c818358f96be8496d03d8f1d85cc6f00efcb65d
smileperez <aa.efimenko@yandex.ru>
2023-10-03 23:53:59 +0300

    Начата работа над popup добавления Halls: создан контроллер, ресурс, реквесты валидации, модель Halls

---
+++ app/Http/Resources/HallsResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class HallsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'rows' => $this->rows,
+            'seats' => $this->seats,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s')
+        ];
+    }
+}
