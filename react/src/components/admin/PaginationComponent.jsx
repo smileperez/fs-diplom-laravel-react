019475da29f2aa693e75022a2c8d7e25296c038d
smileperez <aa.efimenko@yandex.ru>
2023-10-05 00:54:00 +0300

    Мелкие фиксы

--- react/src/components/admin/PaginationComponent.jsx
+++ react/src/components/admin/PaginationComponent.jsx
@@ -10,19 +10,19 @@ export default function PaginationComponent({ meta, onPageClick }) {
     }
 
     return (
-        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 md:px-6">
+        <div className="flex items-center justify-between border-t border-gray-200 bg-white py-3">
             <div className="flex flex-1 justify-between md:hidden">
                 <a
                     href="#"
                     onClick={event => onClick(event, meta.links[0])}
-                    className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium transition duration-500 hover:text-white hover:bg-gray-700 active:bg-[#89639e] active:duration-0"
+                    className="relative inline-flex items-center rounded-md border border-gray-300 bg-[#63536C] text-gray-300 px-4 py-2 text-sm font-medium transition duration-500 hover:text-white hover:bg-gray-700 active:bg-[#89639e] active:duration-0"
                 >
                     Назад
                 </a>
                 <a
                     href="#"
                     onClick={event => onClick(event, meta.links[meta.links.length - 1])}
-                    className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium transition duration-500 hover:text-white hover:bg-gray-700 active:bg-[#89639e] active:duration-0"
+                    className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-[#63536C] text-gray-300 px-4 py-2 text-sm font-medium transition duration-500 hover:text-white hover:bg-gray-700 active:bg-[#89639e] active:duration-0"
                 >
                     Вперёд
                 </a>
@@ -48,14 +48,14 @@ export default function PaginationComponent({ meta, onPageClick }) {
                                     aria-current="page"
                                     className={
                                         `
-                                    "relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20 ` +
+                                    "relative z-10 inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20 transition duration-500 ` +
                                         (idx === 0 ? "rounded-l-md " : "") +
                                         (idx === meta.links.length - 1
                                             ? "rounded-r-md "
                                             : "") +
                                         (link.active
-                                            ? "bg-[#89639e] text-white transition duration-500 "
-                                            : "hover:bg-gray-700 hover:text-white active:bg-[#89639e] active:duration-0")
+                                            ? "bg-[#89639e] text-white duration-0 "
+                                            : "bg-[#63536C] text-gray-300 hover:bg-gray-700 hover:text-white active:bg-[#89639e] ")
                                     }
                                     dangerouslySetInnerHTML={
                                         idx === 0
