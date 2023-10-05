3c11b2d5b3d2c7ad35985aa7630051b98dc6cdd3
smileperez <aa.efimenko@yandex.ru>
2023-10-06 02:37:55 +0300

    добавлен ресурс для Types

---
+++ app/Http/Resources/TypesResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TypesResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'type' => $this->type,
+            'color' => $this->color,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s')
+        ];
+    }
+}
