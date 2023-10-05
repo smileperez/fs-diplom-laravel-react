617855ff6e7ecc716f71654f55c6cb04ca1e0c69
smileperez <aa.efimenko@yandex.ru>
2023-10-06 02:56:42 +0300

    Снова готово CREATE и READ

---
+++ react/src/components/admin/TypesItem.jsx
@@ -0,0 +1,184 @@
+import SlidePopupComponent from "../core/SlidePopupComponent";
+import EButton from "../core/EButton";
+import ESelection from "../core/ESelection";
+import { useState } from "react";
+import {
+    AdjustmentsHorizontalIcon,
+    CloudArrowUpIcon,
+    TrashIcon,
+    XCircleIcon,
+    XMarkIcon,
+} from "@heroicons/react/24/outline";
+import axiosClient from "../../axios";
+
+export default function SeatTypesItem({ seatType, getSeatTypes }) {
+    // Открытие/Закрытие SlidePopupComponent для изменения зала
+    const [change, setChange] = useState(false);
+    // Открытие/Закрытие SlidePopupComponent для удаления зала
+    const [del, setDel] = useState(false);
+
+    // Состояния для изменения зала
+    const [updatedSeatType, setUpdatedSeatType] = useState({
+        type: seatType.type,
+        color: seatType.color,
+    });
+
+    // Состояние для хранения ошибки
+    const [error, setError] = useState("");
+
+    // Отправка put-request в БД c изменениями зала
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const payload = { ...updatedSeatType };
+        console.log(payload);
+        axiosClient.put(`/seattypes/${seatType.id}`, payload);
+    };
+
+    // FIXME:
+    // Функция удаления зала
+    // const onClickDelete = (event) => {
+    //     axiosClient.delete(`/seattypes/${seatType.id}`).then((response) => {
+    //         // Закрываем slider-popup
+    //         setChange(false);
+    //         // Заново перезагружаем из БД все фильмы
+    //         getSeatTypes();
+    //     });
+    // };
+
+    return (
+        <>
+            <section className="mb-4 flex h-auto">
+                <div className="flex flex-1 justify-between h-18 ml-2 p-2 bg-[#F1EBE6]/95 rounded">
+                    <div className="flex">
+                        <div className="w-auto">
+                            <h2 className="text-sm font-light">
+                                ID: <ESelection>{seatType.id}</ESelection>
+                            </h2>
+                            <h2 className="text-sm font-light">
+                                Название:{" "}
+                                <ESelection>{seatType.type}</ESelection>
+                            </h2>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+                        <EButton circle onClick={() => setChange(true)}>
+                            <AdjustmentsHorizontalIcon className="w-6 h-7" />
+                        </EButton>
+                        <EButton
+                            circle
+                            color="danger"
+                            onClick={() => setDel(true)}
+                        >
+                            <TrashIcon className="w-6 h-7" />
+                        </EButton>
+                    </div>
+                </div>
+            </section>
+
+            {/* Slide-Popup для ИЗМЕНЕНИЯ  фильма */}
+            <SlidePopupComponent
+                open={change}
+                setOpen={setChange}
+                title={`Изменение типа №` + seatType.id}
+            >
+                {error && (
+                    <div className="bg-red-500 text-white text-sm py-2 px-2 mb-1 rounded">
+                        {error}
+                    </div>
+                )}
+
+                <form onSubmit={onSubmit} action="#" method="POST">
+                    {/* Название типа места */}
+                    <div>
+                        <label
+                            htmlFor="type"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Название типа места
+                            <span className="text-red-500">*</span>
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="text"
+                                id="type"
+                                name="type"
+                                value={updatedSeatType.type}
+                                onChange={(event) =>
+                                    setUpdatedSeatType({
+                                        ...updatedSeatType,
+                                        type: event.target.value,
+                                    })
+                                }
+                                className="block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#63536C] sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    {/* Название типа места */}
+
+                    {/* Цвет места */}
+                    <div className="mt-2">
+                        <label
+                            htmlFor="color"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Цвет места (HEX 6 символов)
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="text"
+                                id="color"
+                                name="color"
+                                value={updatedSeatType.color}
+                                onChange={(event) =>
+                                    setSeatType({
+                                        ...updatedSeatType,
+                                        color: event.target.value,
+                                    })
+                                }
+                                className="block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#63536C] sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    {/* Цвет места */}
+
+                    <div className="flex justify-between pt-4 mt-4 border-t border-gray-200">
+                        <EButton submit color="regular">
+                            <CloudArrowUpIcon className="h-6 w-6 mr-2" />
+                            Изменить
+                        </EButton>
+                        <EButton color="gray" onClick={() => setChange(false)}>
+                            <XCircleIcon className="h-6 w-6 mr-2" />
+                            Отменить
+                        </EButton>
+                    </div>
+                </form>
+            </SlidePopupComponent>
+            {/* Slide-Popup для ИЗМЕНЕНИЯ  фильма */}
+
+            {/* Slide-Popup для УДАЛЕНИЯ  фильма */}
+            <SlidePopupComponent
+                open={del}
+                setOpen={setDel}
+                title="Удаление места"
+            >
+                <div className="block text-sm font-medium leading-6 text-gray-900">
+                    Вы действительно хотите удалить тип{" "}
+                    <ESelection>№{seatType.id}</ESelection> ?
+                </div>
+
+                <div className="flex justify-between pt-4 mt-4 border-t border-gray-200">
+                    <EButton color="danger" onClick="#">
+                        <TrashIcon className="h-6 w-6 mr-2" />
+                        Удалить
+                    </EButton>
+                    <EButton color="gray" onClick={() => setDel(false)}>
+                        <XCircleIcon className="h-6 w-6 mr-2" />
+                        Отменить
+                    </EButton>
+                </div>
+            </SlidePopupComponent>
+            {/* Slide-Popup для УДАЛЕНИЯ  фильма */}
+        </>
+    );
+}
---
+++ react/src/views/admin/Types.jsx
@@ -0,0 +1,180 @@
+import { useEffect, useState } from "react";
+import PageComponent from "../../components/admin/PageComponent";
+import EButton from "../../components/core/EButton";
+import { PlusCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import SlidePopupComponent from "../../components/core/SlidePopupComponent";
+import axiosClient from "../../axios";
+import PaginationComponent from "../../components/admin/PaginationComponent";
+import TypesItem from "../../components/admin/TypesItem";
+
+export default function SeatTypes() {
+    // Состояние для загрузки из БД общего списка типов мест
+    const [types, setTypes] = useState([]);
+
+    // Соятоние загрузки данных из БД
+    const [loading, setLoading] = useState(false);
+
+    // Соятоние для meta, полученной с ответом на запрос данных из БД (для pagination)
+    const [meta, setMeta] = useState({});
+
+    // Состояния для открытия/закрытия в SlidePopupComponent
+    const [open, setOpen] = useState(false);
+
+    // Состояния для добавления нового типа места
+    const [newType, setNewType] = useState({
+        type: "",
+        color: "63536C",
+    });
+
+    // Состояние для хранения ошибки
+    const [error, setError] = useState("");
+
+    // Функция получения актуальных URL для пагинации из БД (для компонента PaginationComponent)
+    const getTypes = (url) => {
+        url = url || "/types";
+        setLoading(true);
+        axiosClient.get(url).then(({ data }) => {
+            setTypes(data.data);
+            setMeta(data.meta);
+            setLoading(false);
+        });
+    };
+
+    // При каждом обновлении страницы обновляем URL страниц пагинации (для компонента PaginationComponent)
+    useEffect(() => {
+        getTypes();
+    }, []);
+
+    // Callback для пагинации (компонент PaginationComponent)
+    const onPageClick = (link) => {
+        getTypes(link.url);
+    };
+
+    // Отправка request в БД с новым типом места
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const payload = { ...newType };
+        axiosClient
+            .post("/types", payload)
+            .then((response) => {
+                console.log(response);
+                // Закрываем slider-popup
+                setOpen(false);
+                // Перезагружаем страницу
+                getTypes();
+            })
+            .catch((err) => {
+                if (err && err.response) {
+                    // Записываем error в состояние
+                    setError(err.response.data.message);
+                }
+                console.log(err, err.response);
+            });
+    };
+
+    return (
+        <PageComponent
+            title="Настройка мест"
+            button={
+                <EButton color="regular" onClick={() => setOpen(true)}>
+                    <PlusCircleIcon className="h-6 w-6" />
+                    <div className="hidden md:ml-2 md:block">
+                        Добавить место
+                    </div>
+                </EButton>
+            }
+        >
+            {loading && (
+                <div className="text-center text-lg">Загрузка данных...</div>
+            )}
+
+            {!loading && (
+                <div>
+                    {/* // TODO: */}
+                    {types.map((item) => (
+                        <TypesItem seatType={item} getTypes={getTypes} key={item.id} />
+                    ))}
+                    <PaginationComponent
+                        meta={meta}
+                        onPageClick={onPageClick}
+                    />
+                </div>
+            )}
+
+            {/* Slide-Popup для ДОБАВЛЕНИЯ нового типа места */}
+            <SlidePopupComponent
+                open={open}
+                setOpen={setOpen}
+                title="Добавление нового места"
+            >
+                <form onSubmit={onSubmit} action="#" method="POST">
+                    {/* Название типа места */}
+                    <div>
+                        <label
+                            htmlFor="type"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Название типа места{" "}
+                            <span className="text-red-500">*</span>
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="text"
+                                id="type"
+                                name="type"
+                                value={newType.type}
+                                onChange={(event) =>
+                                    setNewType({
+                                        ...newType,
+                                        type: event.target.value,
+                                    })
+                                }
+                                className="block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#63536C] sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    {/* Название типа места */}
+
+                    {/* Цвет места */}
+                    <div className="mt-2">
+                        <label
+                            htmlFor="color"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Цвет места (HEX 6 символов)
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                type="text"
+                                id="color"
+                                name="color"
+                                value={newType.color}
+                                onChange={(event) =>
+                                    setNewType({
+                                        ...newType,
+                                        color: event.target.value,
+                                    })
+                                }
+                                className="block w-full rounded border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#63536C] sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+                    {/* Цвет места */}
+
+                    <div className="flex justify-between pt-4 mt-4 border-t border-gray-200">
+                        <EButton submit>
+                            <PlusCircleIcon className="h-6 w-6 mr-2" />
+                            Добавить
+                        </EButton>
+
+                        <EButton color="gray" onClick={() => setOpen(false)}>
+                            <XCircleIcon className="h-6 w-6 mr-2" />
+                            Отменить
+                        </EButton>
+                    </div>
+                </form>
+            </SlidePopupComponent>
+        </PageComponent>
+    );
+}
