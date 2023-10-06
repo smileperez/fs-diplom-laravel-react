c1e97574e34156636b0b8d95e2666ff235fafcfa
smileperez <aa.efimenko@yandex.ru>
2023-10-06 19:20:43 +0300

    Работа над матрицей

--- react/src/components/admin/MatrixComponent.jsx
+++ react/src/components/admin/MatrixComponent.jsx
@@ -1,26 +1,38 @@
 import { useState } from "react";
-import ESeat from "../core/ESeat";
+
+const makeMatrix = (rows, seats) => {
+    const ans = [];
+    for (let y = 0; y < seats; y++) {
+        const row = Array.from({ length: rows }, (__, x) => {
+            return { y, x };
+        });
+        ans.push(row);
+    }
+    return ans;
+};
 
 export default function MatrixComponent({ rows, seats }) {
-    const makeMatrix = (rows, seats) => {
-        const ans = [];
-        for (let y = 0; y < rows; y++) {
-            const row = Array.from({ length: seats }, (__, x) => {
-                return { x, y };
-            });
-            ans.push(row);
-        }
-        return ans;
-    };
-
+    const [coords, setCoords] = useState({ x: -1, y: -1 });
     const matrix = makeMatrix(rows, seats);
+
+    console.log(coords);
+
+    function onMouseEnter(event, x, y) {
+        setCoords({ x, y });
+    }
 
     return (
         <div>
             {matrix.map((row, i) => (
                 <div key={i}>
                     {row.map((item, j) => (
-                        <ESeat key={j} />
+                        <div
+                            className="inline-block cursor-pointer w-[20px] h-[20px] border border-gray-400 rounded ml-[8px] bg-[#63536C]"
+                            key={j}
+                            onClick={(event) =>
+                                onMouseEnter(event, item.y, item.x)
+                            }
+                        ></div>
                     ))}
                 </div>
             ))}
