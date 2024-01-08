b84e33752ac4f3a668f38d9b7dca0d53bc3f8df7
smileperez <aa.efimenko@efimen.ru>
2024-01-08 21:22:09 +0300

    Исправлен flex кнопки на больших экранах

--- react/src/components/admin/PageComponent.jsx
+++ react/src/components/admin/PageComponent.jsx
@@ -4,7 +4,7 @@ export default function PageComponent({ title, button = "", children }) {
     return (
         <>
             <header className="bg-white shadow">
-                <div className="flex justify-between items-center x-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <div className="flex justify-between items-center x-auto px-4 py-6 sm:px-6 lg:px-8">
                     <h1 className="text-3xl font-bold tracking-tight text-gray-900">
                         {title}
                     </h1>
