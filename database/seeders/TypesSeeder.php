a7c27384e8a61024bc4e52335e3e414eb56c2212
smileperez <aa.efimenko@efimen.ru>
2024-01-08 23:14:36 +0300

    Создан seeder на авто-заполнение таблицы types (типы сидушек)

---
+++ database/seeders/TypesSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypesSeeder extends Seeder
+{
+    /**
+     * Предзаполняем таблицу с типами мест
+     */
+    public function run(): void
+    {
+        DB::table('types')->insert([
+            'type' => 'Обычное',
+            'color' => '63536C',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('types')->insert([
+            'type' => 'VIP',
+            'color' => 'FFD700',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('types')->insert([
+            'type' => 'Заблокированное',
+            'color' => '808080',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
